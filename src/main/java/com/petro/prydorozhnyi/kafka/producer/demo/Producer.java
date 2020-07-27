1d0b6c3da9d86e105f44824e2f4c279f4f20029f
PPrydorozhnyi <Peter35128@gmail.com>
2020-07-27 18:37:14 +0300

    added key

--- src/main/java/com/petro/prydorozhnyi/kafka/producer/demo/Producer.java
+++ src/main/java/com/petro/prydorozhnyi/kafka/producer/demo/Producer.java
@@ -30,19 +30,23 @@ public class Producer {
                 StringSerializer.class.getName());
         //producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-        //producer record
-        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic",
-                "hello, guys");
-        // send data
-        producer.send(record, (recordMetadata, e) -> {
-            if (Objects.isNull(e)) {
-                log.info("Received metadata. Topic: {}. Partition: {}. Offsets: {}. Timestamp: {}",
-                        recordMetadata.topic(),
-                        recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp());
-            } else {
-                log.error("Error during producing", e);
-            }
-        });
+
+        //produce records
+        for (int i = 0; i < 10; i++) {
+            String key = "id_" + i;
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", key,
+                    "hello, guys " + i);
+            // send data
+            producer.send(record, (recordMetadata, e) -> {
+                if (Objects.isNull(e)) {
+                    log.info("Received metadata. Topic: {}. Partition: {}. Offsets: {}. Timestamp: {}.",
+                            recordMetadata.topic(),
+                            recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp());
+                } else {
+                    log.error("Error during producing", e);
+                }
+            });
+        }
         //flush and close producer
         producer.close();
     }
